204a82aa2f889c44226ee19ddf960508abb22928
scnerd <scnerd@gmail.com>
2014-04-12 17:23:08 -0700

    Initial commit

---
+++ SystemChangeReporter/frmMain.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace SystemChangeReporter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fsWatcher = new System.IO.FileSystemWatcher();
+            this.treFiles = new System.Windows.Forms.TreeView();
+            this.tblLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.txtRegLog = new System.Windows.Forms.TextBox();
+            this.treReg = new System.Windows.Forms.TreeView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtFileLog = new System.Windows.Forms.TextBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.cmbDrive = new System.Windows.Forms.ToolStripComboBox();
+            this.btnSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageDriveFiltersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageRegistryFiltersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chkDriveMonitor = new System.Windows.Forms.ToolStripMenuItem();
+            this.chkRegistryMonitor = new System.Windows.Forms.ToolStripMenuItem();
+            this.registryPollTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtPollTime = new System.Windows.Forms.ToolStripTextBox();
+            this.tmrPollRegistry = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fsWatcher)).BeginInit();
+            this.tblLayout.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fsWatcher
+            // 
+            this.fsWatcher.EnableRaisingEvents = true;
+            this.fsWatcher.IncludeSubdirectories = true;
+            this.fsWatcher.SynchronizingObject = this;
+            this.fsWatcher.Changed += new System.IO.FileSystemEventHandler(this.fsWatcher_Changed);
+            this.fsWatcher.Created += new System.IO.FileSystemEventHandler(this.fsWatcher_Changed);
+            this.fsWatcher.Deleted += new System.IO.FileSystemEventHandler(this.fsWatcher_Changed);
+            this.fsWatcher.Renamed += new System.IO.RenamedEventHandler(this.fsWatcher_Changed);
+            // 
+            // treFiles
+            // 
+            this.treFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treFiles.Enabled = false;
+            this.treFiles.Location = new System.Drawing.Point(3, 3);
+            this.treFiles.Name = "treFiles";
+            this.treFiles.Size = new System.Drawing.Size(342, 252);
+            this.treFiles.TabIndex = 0;
+            // 
+            // tblLayout
+            // 
+            this.tblLayout.ColumnCount = 2;
+            this.tblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblLayout.Controls.Add(this.txtRegLog, 1, 1);
+            this.tblLayout.Controls.Add(this.treReg, 1, 0);
+            this.tblLayout.Controls.Add(this.treFiles, 0, 0);
+            this.tblLayout.Controls.Add(this.btnStart, 0, 2);
+            this.tblLayout.Controls.Add(this.btnStop, 1, 2);
+            this.tblLayout.Controls.Add(this.txtFileLog, 0, 1);
+            this.tblLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblLayout.Location = new System.Drawing.Point(0, 27);
+            this.tblLayout.Name = "tblLayout";
+            this.tblLayout.RowCount = 3;
+            this.tblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.89909F));
+            this.tblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.10092F));
+            this.tblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tblLayout.Size = new System.Drawing.Size(696, 362);
+            this.tblLayout.TabIndex = 1;
+            // 
+            // txtRegLog
+            // 
+            this.txtRegLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtRegLog.Location = new System.Drawing.Point(351, 261);
+            this.txtRegLog.Multiline = true;
+            this.txtRegLog.Name = "txtRegLog";
+            this.txtRegLog.ReadOnly = true;
+            this.txtRegLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRegLog.Size = new System.Drawing.Size(342, 63);
+            this.txtRegLog.TabIndex = 5;
+            // 
+            // treReg
+            // 
+            this.treReg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treReg.Enabled = false;
+            this.treReg.Location = new System.Drawing.Point(351, 3);
+            this.treReg.Name = "treReg";
+            this.treReg.Size = new System.Drawing.Size(342, 252);
+            this.treReg.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStart.Location = new System.Drawing.Point(3, 330);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(342, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Reset Changes";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStop.Location = new System.Drawing.Point(351, 330);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(342, 29);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Freeze Changes";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtFileLog
+            // 
+            this.txtFileLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFileLog.Location = new System.Drawing.Point(3, 261);
+            this.txtFileLog.Multiline = true;
+            this.txtFileLog.Name = "txtFileLog";
+            this.txtFileLog.ReadOnly = true;
+            this.txtFileLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFileLog.Size = new System.Drawing.Size(342, 63);
+            this.txtFileLog.TabIndex = 4;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmbDrive,
+            this.btnSave,
+            this.filterToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(696, 27);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // cmbDrive
+            // 
+            this.cmbDrive.Name = "cmbDrive";
+            this.cmbDrive.Size = new System.Drawing.Size(121, 23);
+            this.cmbDrive.Text = "Drive";
+            this.cmbDrive.SelectedIndexChanged += new System.EventHandler(this.cmbDrive_SelectedIndexChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(43, 23);
+            this.btnSave.Text = "Save";
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageDriveFiltersToolStripMenuItem,
+            this.manageRegistryFiltersToolStripMenuItem});
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(45, 23);
+            this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // manageDriveFiltersToolStripMenuItem
+            // 
+            this.manageDriveFiltersToolStripMenuItem.Name = "manageDriveFiltersToolStripMenuItem";
+            this.manageDriveFiltersToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.manageDriveFiltersToolStripMenuItem.Text = "Manage Drive Filters";
+            // 
+            // manageRegistryFiltersToolStripMenuItem
+            // 
+            this.manageRegistryFiltersToolStripMenuItem.Name = "manageRegistryFiltersToolStripMenuItem";
+            this.manageRegistryFiltersToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.manageRegistryFiltersToolStripMenuItem.Text = "Manage Registry Filters";
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chkDriveMonitor,
+            this.chkRegistryMonitor,
+            this.registryPollTimeToolStripMenuItem});
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(107, 23);
+            this.stopToolStripMenuItem.Text = "Toggle Monitors";
+            // 
+            // chkDriveMonitor
+            // 
+            this.chkDriveMonitor.Checked = true;
+            this.chkDriveMonitor.CheckOnClick = true;
+            this.chkDriveMonitor.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDriveMonitor.Name = "chkDriveMonitor";
+            this.chkDriveMonitor.Size = new System.Drawing.Size(169, 22);
+            this.chkDriveMonitor.Text = "Drive Monitor";
+            // 
+            // chkRegistryMonitor
+            // 
+            this.chkRegistryMonitor.Checked = true;
+            this.chkRegistryMonitor.CheckOnClick = true;
+            this.chkRegistryMonitor.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRegistryMonitor.Name = "chkRegistryMonitor";
+            this.chkRegistryMonitor.Size = new System.Drawing.Size(169, 22);
+            this.chkRegistryMonitor.Text = "Registry Monitor";
+            // 
+            // registryPollTimeToolStripMenuItem
+            // 
+            this.registryPollTimeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtPollTime});
+            this.registryPollTimeToolStripMenuItem.Name = "registryPollTimeToolStripMenuItem";
+            this.registryPollTimeToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.registryPollTimeToolStripMenuItem.Text = "Registry Poll Time";
+            // 
+            // txtPollTime
+            // 
+            this.txtPollTime.Name = "txtPollTime";
+            this.txtPollTime.Size = new System.Drawing.Size(100, 23);
+            this.txtPollTime.TextChanged += new System.EventHandler(this.txtPollTime_TextChanged);
+            // 
+            // tmrPollRegistry
+            // 
+            this.tmrPollRegistry.Tick += new System.EventHandler(this.tmrPollRegistry_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 389);
+            this.Controls.Add(this.tblLayout);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fsWatcher)).EndInit();
+            this.tblLayout.ResumeLayout(false);
+            this.tblLayout.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fsWatcher;
+        private System.Windows.Forms.TableLayoutPanel tblLayout;
+        private System.Windows.Forms.TreeView treFiles;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TreeView treReg;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripComboBox cmbDrive;
+        private System.Windows.Forms.TextBox txtRegLog;
+        private System.Windows.Forms.TextBox txtFileLog;
+        private System.Windows.Forms.ToolStripMenuItem btnSave;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageDriveFiltersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageRegistryFiltersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chkDriveMonitor;
+        private System.Windows.Forms.ToolStripMenuItem chkRegistryMonitor;
+        private System.Windows.Forms.ToolStripMenuItem registryPollTimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox txtPollTime;
+        private System.Windows.Forms.Timer tmrPollRegistry;
+    }
+}
+
---
+++ SystemChangeReporter/frmMain.cs
@@ -0,0 +1,269 @@
+﻿/*
+ * TODO: Get registry monitoring working (have "Current_User -> Environment -> Junk" key to play with)
+ *   - Get updating for current keys
+ *   - Detect adding/deleting keys
+ * TODO(?): Implement tree view
+ * TODO: Implement filtering dialogs
+ * TODO: Implement exclusion filtering
+ * TODO: Create documentation
+ * TODO(?): Implement resetting (start) and freezing (stop)
+ * TODO(?): Implement saving of data
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SystemChangeReporter
+{
+    public partial class frmMain : Form
+    {
+        private const string EOL = "\r\n";
+        private const string TIME_FORMAT = "HH:mm:ss:ffff: ";
+        private enum ChangeType
+        {
+            Change,
+            Create,
+            Delete,
+            Rename,
+            Unknown
+        }
+        private RegistryKey[] RegistryBases = { /*Registry.ClassesRoot, */Registry.CurrentConfig, Registry.CurrentUser, Registry.LocalMachine, Registry.PerformanceData, Registry.Users };
+
+        Dictionary<string, Dictionary<string, object>> PrevRegistry = new Dictionary<string, Dictionary<string, object>>();
+        int RegistryPollTime = 5000;
+        bool IsRunning = false;
+
+        #region SETUP
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDrive.Items.Clear();
+            cmbDrive.Items.AddRange(Environment.GetLogicalDrives().Select(t => t.ToString()).ToArray());
+            if (cmbDrive.Items.Contains(@"C:\"))
+                cmbDrive.SelectedItem = @"C:\";
+            else
+                cmbDrive.SelectedIndex = 0;
+
+            txtPollTime.Text = RegistryPollTime.ToString();
+            tmrPollRegistry.Interval = RegistryPollTime;
+
+            IsRunning = true;
+        }
+
+        #endregion
+
+        #region EVENTS
+
+        private void cmbDrive_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ResetDriveChanges();
+            fsWatcher.Path = cmbDrive.SelectedItem.ToString();
+        }
+
+        private void fsWatcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            ChangeFile(e.FullPath, ConvertFSChangeToChange(e.ChangeType));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ResetDriveChanges();
+            ResetRegistryChanges();
+            IsRunning = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            IsRunning = false;
+        }
+
+        private void txtPollTime_TextChanged(object sender, EventArgs e)
+        {
+            int NewTime = RegistryPollTime;
+            if(int.TryParse(txtPollTime.Text, out NewTime))
+                RegistryPollTime = NewTime;
+            txtPollTime.Text = RegistryPollTime.ToString();
+            tmrPollRegistry.Interval = RegistryPollTime;
+        }
+
+        private void tmrPollRegistry_Tick(object sender, EventArgs e)
+        {
+            if (!(IsRunning && chkRegistryMonitor.Checked))
+                return;
+
+
+        }
+
+        #endregion
+
+        #region HELPERS
+
+        private ChangeType ConvertFSChangeToChange(System.IO.WatcherChangeTypes From)
+        {
+            switch (From)
+            {
+                case System.IO.WatcherChangeTypes.Changed:
+                    return ChangeType.Change;
+                case System.IO.WatcherChangeTypes.Created:
+                    return ChangeType.Create;
+                case System.IO.WatcherChangeTypes.Deleted:
+                    return ChangeType.Delete;
+                case System.IO.WatcherChangeTypes.Renamed:
+                    return ChangeType.Rename;
+                default:
+                    return ChangeType.Unknown;
+            }
+        }
+
+        //private void ResetRegistryMonitors()
+        //{
+        //    Monitors = new List<RegistryMonitor>();
+        //    RegistryMonitor Mon;
+
+        //    RegistryLog("Starting registry dive");
+
+        //    List<RegistryKey> AllKeys = new List<RegistryKey>();
+        //    foreach (RegistryKey CurBase in RegistryBases)
+        //        GetRegKeys(CurBase, ref AllKeys, 1);
+
+        //    RegistryLog("Midway through dive");
+
+        //    foreach (RegistryKey Key in AllKeys)
+        //    {
+        //        Mon = new RegistryMonitor(Key);
+        //        Mon.RegChanged += (s, e) => ChangeRegistry(Key.Name, ChangeType.Change);
+        //        Mon.Start();
+        //        Monitors.Add(Mon);
+        //    }
+
+        //    RegistryLog("Finishing registry dive");
+        //}
+
+        //private int GetRegKeys(RegistryKey Base, ref List<RegistryKey> AllKeys, int Depth)
+        //{
+        //    var v = 1;
+        //    AllKeys.Add(Base);
+        //    if (Depth < RegistryDepth)
+        //        foreach (var SubKeyName in Base.GetSubKeyNames())
+        //            if (SubKeyName != "*")
+        //            {
+        //                var SubKey = Base.OpenSubKey(SubKeyName);
+        //                if (AllKeys.Contains(SubKey))
+        //                    throw new InvalidOperationException("Ran into the same key again?");
+        //                v += GetRegKeys(SubKey, ref AllKeys, Depth + 1);
+        //            }
+        //    RegistryLog(Base.Name + " : " + v);
+        //    return v;
+        //}
+
+        private void ResetDriveChanges()
+        {
+            txtFileLog.Text = "";
+            treFiles.Nodes.Clear();
+        }
+
+        private void ChangeFile(string FilePath, ChangeType Chng)
+        {
+            if (IsRunning && chkDriveMonitor.Checked)
+                FileLog("File changed (" + Chng.ToString() + "): " + FilePath);
+        }
+
+        private void FileLog(string Text)
+        {
+            txtFileLog.AppendText(DateTime.Now.ToString(TIME_FORMAT) + Text + EOL);
+        }
+
+        private void PollRegistry()
+        {
+            List<string> CheckedStrs = new List<string>(PrevRegistry.Count);
+            foreach (RegistryKey Root in RegistryBases)
+                UpdateRegKey(Root, ref CheckedStrs);
+            var Deleted =
+                from key in PrevRegistry.Keys
+                where !CheckedStrs.Contains(key)
+                select key;
+            foreach (var DeletedKey in Deleted)
+                ChangeRegistry(DeletedKey, ChangeType.Delete);
+        }
+
+        private void UpdateRegKey(RegistryKey RootKey, ref List<string> CheckedStrs)
+        {
+            CheckedStrs.Add(RootKey.Name);
+
+            if (!PrevRegistry.ContainsKey(RootKey.Name))
+            {
+                ChangeRegistry(RootKey, ChangeType.Create);
+                PrevRegistry.Add(RootKey.Name, new Dictionary<string, object>());
+            }
+
+            foreach (var key in RootKey.GetValueNames())
+            {
+                var CurrentValue = RootKey.GetValue(key);
+                var CurrentKVP = new KeyValuePair<string, object>(key, CurrentValue);
+                var StoredKVP = PrevRegistry[RootKey.Name].FirstOrDefault(kvp => kvp.Key == key);
+                if (StoredKVP.Equals(default(KeyValuePair<string, object>)))
+                {
+                    ChangeRegistry(RootKey, ChangeType.Create, CurrentKVP);
+                    PrevRegistry[RootKey.Name].Add(key, CurrentValue);
+                }
+                else if (StoredKVP.Value != CurrentValue)
+                {
+                    ChangeRegistry(RootKey, ChangeType.Change, new KeyValuePair<string, object>(key, CurrentValue));
+                    PrevRegistry[RootKey.Name][key] = CurrentValue;
+                }
+            }
+
+            foreach (var SubKeyName in RootKey.GetSubKeyNames())
+            {
+                UpdateRegKey(RootKey.OpenSubKey(SubKeyName), ref CheckedStrs);
+            }
+
+            RootKey.Close();
+        }
+
+        private string KeyValueName(string RegistryKeyName, string KeyValueName)
+        {
+            return RegistryKeyName + '.' + KeyValueName;
+        }
+
+        private void ResetRegistryChanges()
+        {
+            txtRegLog.Text = "";
+            treReg.Nodes.Clear();
+        }
+
+        private void ChangeRegistry(RegistryKey RegKey, ChangeType Chng, KeyValuePair<string, object> ChangedValue = default(KeyValuePair<string, object>))
+        {
+            ChangeRegistry(RegKey.Name, Chng, ChangedValue);
+        }
+
+        private void ChangeRegistry(string KeyPath, ChangeType Chng, KeyValuePair<string, object> ChangedValue = default(KeyValuePair<string, object>))
+        {
+            if (IsRunning && chkRegistryMonitor.Checked)
+                RegistryLog("Registry changed (" + Chng.ToString() + "): " + KeyPath +
+                    (ChangedValue.Equals(default(KeyValuePair<string, object>)) ? "" : " -> " + ChangedValue.Key + ":" + ChangedValue.Value));
+        }
+
+        private void RegistryLog(string Text)
+        {
+            txtRegLog.AppendText(DateTime.Now.ToString(TIME_FORMAT) + Text + EOL);
+        }
+
+        #endregion
+
+
+    }
+}
